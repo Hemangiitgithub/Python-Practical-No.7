1e2fca31b75487c57f32b32fd8d0f538465ffe54
Hemangiitgithub <103062750+Hemangiitgithub@users.noreply.github.com>
2022-07-12 18:36:58 +0530

    Add files via upload

---
+++ Q5.Multipy 2 list.py
@@ -0,0 +1,12 @@
+# Q.5 Python Program to multiply two list
+
+import numpy as np
+
+list1 = list(map(int,input().split()))
+list2 = list(map(int,input().split()))
+
+product = np.multiply(list1,list2)
+
+print(product)
+
+
